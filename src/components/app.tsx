4853e58b51a392d6bc4b955b7188aabc19722324
Kent Richards <info@kentrichards.net>
2022-09-02 12:48:38 -0600

    Remove

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,10 @@
 import { FunctionalComponent } from 'preact';
 
-import Header from './header';
 import Chart from "./Chart";
 
 const App: FunctionalComponent = () => {
     return (
         <div id="preact_root">
-            <Header />
             <Chart />
         </div>
     );
